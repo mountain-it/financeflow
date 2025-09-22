1f83b0ad19215c3a8111cbf5c2f20427845476d0
mountain <tech.mountain.investment@gmail.com>
2025-09-22 10:17:08 +0100

    Add Vercel Analytics integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Routes from "./Routes";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <Analytics />
+    </>
   );
 }
 
